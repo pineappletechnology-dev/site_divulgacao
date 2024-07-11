a8e38425b6cfea0feeead850bf23a81c883e3f14
RodrigoTheDev <rocapucho123123@gmail.com>
2024-07-11 11:21:36 -0300

    fix: tamanhos do footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,11 @@ export default function RootLayout({
   return (
     
     <html lang="en">
-      
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
