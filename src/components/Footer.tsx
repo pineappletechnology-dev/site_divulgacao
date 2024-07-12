428d1416df97f307ea8fb5a05720d7a6261b3929
Liandra Meirelles <96798851+MeirellesDEV@users.noreply.github.com>
2024-07-11 21:14:02 -0300

    fix: atualização do estilo do footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,10 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from 'next/link';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import '../css/colors.css';
+import styles from '../css/footer.module.css';
 
-import '../css/colors.css'
-import styles from '../css/footer.module.css'
+const inter = Inter({ subsets: ['latin'], weight: '300' });
 
 export default function Footer() {
     return (
@@ -10,7 +12,7 @@ export default function Footer() {
             <div className={styles.footer_layout}>
                 <div className={styles.footer_menu}>
                     <Link href="/">
-                        <Image src="/img/logo.png" width={200} height={200} alt='contato instagram'/>
+                        <Image src="/img/logo.png" width={200} height={200} alt='contato instagram' />
                     </Link>
                     <div className={styles.line}></div>
                     <div className={styles.acesso_rapido}>
@@ -24,25 +26,25 @@ export default function Footer() {
 
                 <div className={styles.footer_frase}>
                     <h1>• <span>Sua</span> visão, <span>nosso</span> código •</h1>
-                    <Link href="/" className={styles.botao}>Fale com um especialista</Link>
+                    <Link href="/" className={`${styles.botao} ${inter.className}`}>Fale com um especialista</Link>
                 </div>
             </div>
 
             <div className={styles.footer_info}>
-                <p>&copy; Todos os Direitos Reservados</p>
-                <p>Desenvolvido por Pineapple Tech</p>
+                <p className={inter.className}>&copy; Todos os Direitos Reservados</p>
+                <p className={inter.className}>Desenvolvido por Pineapple Tech</p>
                 <div className={styles.footer_social_media}>
                     <Link href="#">
-                        <Image src="img/wpp.svg" width={30} height={30} alt='contato whatsapp'/>
+                        <Image src="img/wpp.svg" width={25} height={25} alt='contato whatsapp' />
                     </Link>
                     <Link href="#">
-                        <Image src="img/insta.svg" width={30} height={30} alt='contato instagram'/>
+                        <Image src="img/insta.svg" width={25} height={25} alt='contato instagram' />
                     </Link>
                     <Link href="#">
-                        <Image src="img/linkedin.svg" width={30} height={30} alt='contato linkedin'/>
+                        <Image src="img/linkedin.svg" width={25} height={25} alt='contato linkedin' />
                     </Link>
                 </div>
             </div>
         </div>
-    )
+    );
 }
